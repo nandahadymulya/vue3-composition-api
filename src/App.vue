38b9edade42fd8b9d0a2574cb6a58db9d111c643
Nanda Hady Mulya <nandahadymulyaa@gmail.com>
2022-10-18 08:34:21 +0700

    feat: lifecycle composition api

--- src/App.vue
+++ src/App.vue
@@ -1,70 +1,13 @@
 <script>
-import { ref, reactive, toRefs, watch } from "vue";
+import { onBeforeMount, onMounted, onBeforeUpdate, onUpdated, onBeforeUnmount, onUnmounted, onActivated, onDeactivated, onErrorCaptured } from "vue";
 
 export default {
   setup() {
-    const counter = reactive({
-      value: 0,
-      foo: "bar",
-    });
-
-    const num1 = ref(2);
-    const num2 = ref(3);
-
-    const increment = () => {
-      counter.value++;
-      num1.value++;
-      num2.value++;
-    };
-
-    // gabisa karena object lebih baik refs
-    // watch(counter, (current, before) => {
-    //   console.log(counter.value);
-    //   console.log(current);
-    //   console.log(before);
-    // });
-
-    // ref
-    // watch(num1, (current, before) => {
-    //   console.log(num1.value);
-    //   console.log(current);
-    //   console.log(before);
-    // });
-
-    // ref 2 ref
-    watch([num1, num2], (current, before) => {
-      console.log(num1.value);
-      console.log(num2.value);
-      console.log(current);
-      console.log(before);
-    });
-
-    // for object reactive
-    // watch(
-    //   () => counter.value,
-    //   (current, before) => {
-    //     console.log(counter.value);
-    //     console.log(current);
-    //     console.log(before);
-    //   }
-    // );
-
-    return {
-      ...toRefs(counter),
-      num1,
-      increment,
-    };
+    return {};
   },
 };
 </script>
 
 <template>
-  <div>
-    <!-- <p>Name: {{ user.name }}</p>
-    <p>Age: {{ user.age }}</p> -->
-    <p>Count: {{ value }}</p>
-    <p>Count Num1: {{ num1 }}</p>
-    <button @click="increment">Increment</button>
-    <!-- <p>Result: {{ result }}</p> -->
-  </div>
+  <div></div>
 </template>
