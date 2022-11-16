6dc59e0105b263af8a9db0530846c81a33bb7531
Nanda Hady Mulya <nandahadymulyaa@gmail.com>
2022-11-17 01:36:45 +0700

    feat: added todo vue composition API

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,89 @@
 <script>
-import { ref, reactive, toRef } from "vue";
-import UserComponent from "./components/UserComponent.vue";
+import { ref, toRefs, reactive, onMounted, computed } from "vue";
+import TodoList from "./components/TodoList.vue";
 
 export default {
-  components: {
-    UserComponent,
-  },
+  components: { TodoList },
   setup() {
-    const label = ref("Biodata");
-    const user = reactive({
-      name: "Nanda",
-      age: 25,
+    const todo = ref("");
+    const todos = reactive({
+      list: [],
     });
 
-    const changeName = () => {
-      user.name = "Nanda Hady Mulya";
+    onMounted(() => {
+      todos.list = JSON.parse(localStorage.getItem("todos")) || [];
+    });
+
+    const countTodo = computed(() => {
+      return todos.list.length;
+    });
+
+    const createTodo = () => {
+      todos.list.unshift({
+        activity: todo.value,
+        status: false,
+      });
+      todo.value = "";
+      saveToLocal();
     };
 
-    const onSubmit = (val) => {
-      console.log(val);
+    const deleteTodo = (todoIndex) => {
+      todos.list = todos.list.filter((todo, index) => {
+        if (index != todoIndex) {
+          return todo;
+        }
+      });
+      saveToLocal();
+    };
+
+    const doneTodo = (todoIndex) => {
+      todos.list = todos.list.map((todo, index) => {
+        if (index == todoIndex) {
+          todo.status = !todo.status;
+        }
+        return todo;
+      });
+      saveToLocal();
+    };
+
+    const saveToLocal = () => {
+      localStorage.setItem("todos", JSON.stringify(todos.list));
     };
 
     return {
-      label,
-      user,
-      changeName,
-      onSubmit,
+      todo,
+      ...toRefs(todos),
+      countTodo,
+      createTodo,
+      deleteTodo,
+      doneTodo,
     };
   },
 };
 </script>
 
 <template>
-  <h1>Parent</h1>
-  <hr />
-  <UserComponent :label="label" :user="user" class="active" address="padang" @submit="onSubmit">
-    <div>Ini slot</div>
-  </UserComponent>
-  <button @click="changeName">Change Name</button>
+  <div class="container-fluid bg-light rounded-lg vh-100">
+    <div class="row p-5 text-center">
+      <div class="col">
+        <h1 class="fw-bold">Simple Todo App</h1>
+        <p class="text-muted">Developed using Vue 3 Composition API.</p>
+      </div>
+    </div>
+
+    <div class="row justify-content-center">
+      <div class="col-md-10 mb-3">
+        <div class="d-flex gap-1 m-1">
+          <input class="form-control" v-model="todo" @keyup.enter="createTodo" type="text" placeholder="Add new todo" />
+          <button class="btn btn-primary" @click="createTodo">Add</button>
+        </div>
+      </div>
+
+      <div class="col-md-10 mb-3">
+        <div class="badge bg-info float-end m-1">Total: {{ countTodo }}</div>
+      </div>
+
+      <TodoList :todos="list" @parsingIndexDelete="deleteTodo" @parsingIndexDone="doneTodo" />
+    </div>
+  </div>
 </template>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,47 @@
+<script>
+export default {
+  props: {
+    todo: String,
+    todos: {
+      type: Array,
+      default: [],
+    },
+  },
+  setup(props, { emit }) {
+    const parsingIndexDelete = (index) => {
+      emit("parsingIndexDelete", index);
+    };
+
+    const parsingIndexDone = (index) => {
+      emit("parsingIndexDone", index);
+    };
+
+    return {
+      parsingIndexDelete,
+      parsingIndexDone,
+    };
+  },
+};
+</script>
+<template>
+  <div class="col-md-10 mb-3">
+    <ul class="list-group" v-for="(todo, index) in todos" :key="index">
+      <li class="list-group-item m-1">
+        <div class="d-flex justify-content-between align-items-center">
+          <div>
+            <span v-if="todo.status">
+              <del class="text-muted">{{ todo.activity }}</del>
+            </span>
+            <span v-else>
+              <span>{{ todo.activity }}</span>
+            </span>
+          </div>
+          <div>
+            <button class="btn btn-danger btn-sm me-1" @click="parsingIndexDelete(index)">Delete</button>
+            <button class="btn btn-success btn-sm" @click="parsingIndexDone(index)">Done</button>
+          </div>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
